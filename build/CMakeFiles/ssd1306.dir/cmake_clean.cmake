7246d0137b762980900c1dea6e84b046b9c9da12
sqmi_ <ssammie47@gmail.com>
2024-04-09 20:07:26 -0500

    progressed through the assignment

---
+++ build/CMakeFiles/ssd1306.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ssd1306.dir/linux_i2c.c.o"
+  "CMakeFiles/ssd1306.dir/linux_i2c.c.o.d"
+  "CMakeFiles/ssd1306.dir/ssd1306.c.o"
+  "CMakeFiles/ssd1306.dir/ssd1306.c.o.d"
+  "libssd1306.a"
+  "libssd1306.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ssd1306.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
